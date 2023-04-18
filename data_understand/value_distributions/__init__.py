c4b1413f8664616dbd2b77cc2d6c79dee28da0d4
Gaurav Gupta <ggupta2005@gmail.com>
2023-04-18 20:18:46 +0000

    Add value value distribution for continous features

---
+++ data_understand/value_distributions/__init__.py
@@ -0,0 +1,8 @@
+from .histogram_distribution import (
+    generate_histogram_distributions,
+    get_jupyter_nb_code_to_generate_histogram_distributions)
+
+__all__ = [
+    "generate_histogram_distributions",
+    "get_jupyter_nb_code_to_generate_histogram_distributions",
+]
