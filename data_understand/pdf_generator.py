3bdf88b9fe731e4ca2e4ab66ab4c3e45b25169c4
Gaurav Gupta <ggupta2005@gmail.com>
2023-05-10 18:43:18 +0000

    Refactor some PDF generation code
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>

--- data_understand/pdf_generator.py
+++ data_understand/pdf_generator.py
@@ -17,70 +17,65 @@ class PDFReportGenerator(FPDF):
         self._target_column_name = target_column_name
         self._dataframe = load_dataset_as_dataframe(file_name)
 
-    def add_title_and_description(self):
-        # Add the first page
-        self.add_page()
+    def _add_heading(self, message: str):
         self.set_font("Arial", size=20)
-        self.cell(
-            200, 10, "Understanding the data in " + self._file_name, align="C"
-        )
-        self.ln()
-        self.ln()
-        self.set_font("Arial", size=11)
-        self.cell(0, 10, "This PDF has different insights about your dataset.")
+        self.cell(200, 10, message, align="C")
         self.ln()
 
-    def add_index(self):
+    def _add_sub_heading(self, message: str):
+        self.set_font("Arial", size=15)
+        self.cell(None, None, message)
+        self.ln()
+
+    def _add_text(self, message: str, multi_line=False):
+        self.set_font("Arial", size=11)
+        if multi_line:
+            self.multi_cell(0, 10, message)
+        else:
+            self.cell(0, 10, message)
+        self.ln()
+
+    def add_title_and_description_page(self):
+        # Add the first page
+        self.add_page()
+        self._add_heading(
+            message="Understanding the data in " + self._file_name
+        )
+        self._add_text(
+            message="This PDF has different insights about your dataset."
+        )
+
+    def add_index_page(self):
         # Add an index to the document
         self.add_page()
-        self.set_font("Arial", size=15)
-        self.cell(0, 10, "Index")
-        self.ln()
-        self.set_font("Arial", size=12)
-        self.cell(
-            0, 10, "Chapter 1 - Dataset Charateristics", link=self.add_link()
-        )
+        self._add_sub_heading(message="Index")
+        self._add_text("Chapter 1 - Dataset Charateristics")
+        self._add_text("Chapter 2 - Visualize distributions of the dataset")
+        self._add_text("Chapter 3 - Class Imbalance")
 
     def add_data_characteristics_page(self):
         # Add the dataset characteristics page
         self.add_page()
-        self.set_font("Arial", size=20)
-        self.cell(200, 10, "Chapter 1 - Dataset Charateristics", align="C")
-        self.ln()
-        self.set_font("Arial", size=11)
-
-        self.cell(
-            0,
-            10,
+        self._add_heading("Chapter 1 - Dataset Charateristics")
+        self._add_text(
             "The number of rows in the dataset are: "
-            + str(self._dataframe.shape[0]),
+            + str(self._dataframe.shape[0])
         )
-        self.ln()
 
-        self.cell(
-            0,
-            10,
+        self._add_text(
             "The number of columns in the dataset are: "
             + str(self._dataframe.shape[1]),
         )
-        self.ln()
 
-        self.cell(
-            0,
-            10,
+        self._add_text(
             "The name of the target column is: " + self._target_column_name,
         )
-        self.ln()
 
-        self.cell(
-            0,
-            10,
+        self._add_text(
             get_message_columns_having_missing_values(self._dataframe),
         )
-        self.ln()
 
-        self.cell(0, 10, "The table of data type for each column is below:-")
-        self.ln()
+        self._add_text("The table of data type for each column is below:-")
         dataset_snapshot_table = get_column_types_as_tuple(self._dataframe)
         with self.table(text_align="CENTER") as table:
             for data_row in dataset_snapshot_table:
@@ -91,34 +86,18 @@ class PDFReportGenerator(FPDF):
     def add_feature_correlation_page(self):
         # Add a new page
         self.add_page()
-        self.set_font("Arial", size=20)
-        self.cell(
-            200,
-            10,
-            "Chapter 2 - Visualize distributions of the dataset",
-            align=Align.C,
-        )
-        self.ln()
-
-        self.set_font("Arial", size=11)
-        self.multi_cell(
-            0,
-            10,
+        self._add_heading("Chapter 2 - Visualize distributions of the dataset")
+        self._add_text(
             "This section have graphs and tables using which you can "
             "visualize distibutions of different features in your dataset, "
             "visualize the distibution of various categories for "
             "categorical features and find correlations between "
             "different features.",
+            multi_line=True,
         )
-        self.ln()
+        self._add_sub_heading("Feature Correlations")
 
-        self.set_font("Arial", size=15)
-        self.cell(None, None, "Feature Correlations")
-        self.ln()
-
-        self.set_font("Arial", size=11)
-        self.cell(0, 10, "Top five positive feature correlations")
-        self.ln()
+        self._add_text("Top five positive feature correlations")
         positive_feature_correlation_table = get_feature_correlations_as_tuple(
             self._dataframe, 5, True
         )
@@ -129,9 +108,7 @@ class PDFReportGenerator(FPDF):
                     row.cell(datum)
         self.ln()
 
-        self.set_font("Arial", size=11)
-        self.cell(0, 10, "Top five negative feature correlations")
-        self.ln()
+        self._add_text("Top five negative feature correlations")
         negative_feature_correlation_table = get_feature_correlations_as_tuple(
             self._dataframe, 5, False
         )
@@ -143,10 +120,8 @@ class PDFReportGenerator(FPDF):
         self.ln()
 
         self.add_page()
-        self.set_font("Arial", size=11)
-        self.cell(200, 10, "Feature correlation graph for numerical features")
+        self._add_text("Feature correlation graph for numerical features")
         save_correlation_matrices(self._dataframe)
-
         # Add the image to the page
         self.image(
             "correlation.png",
@@ -160,18 +135,13 @@ class PDFReportGenerator(FPDF):
     def add_class_imbalance_page(self):
         # Add a new page
         self.add_page()
-        self.set_font("Arial", size=20)
-        self.cell(200, 10, "Chapter 3 - Class Imbalance", align="C")
-        self.ln()
-        self.set_font("Arial", size=11)
-        self.multi_cell(
-            0,
-            10,
+        self._add_heading("Chapter 3 - Class Imbalance")
+        self._add_text(
             get_message_target_column_imbalance(
                 self._dataframe, self._target_column_name
             ),
+            multi_line=True,
         )
-        self.ln()
 
     def save_pdf(self):
         self.output(self._file_name + ".pdf")
@@ -185,7 +155,8 @@ def generate_pdf(args: Any) -> None:
     pdf_report_generator = PDFReportGenerator(
         args.file_name, args.target_column
     )
-    pdf_report_generator.add_title_and_description()
+    pdf_report_generator.add_title_and_description_page()
+    pdf_report_generator.add_index_page()
     pdf_report_generator.add_data_characteristics_page()
     pdf_report_generator.add_feature_correlation_page()
     pdf_report_generator.add_class_imbalance_page()
