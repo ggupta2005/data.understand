ea0acd076e3912a1d74098641fcc047c5f02bda3
Gaurav Gupta <ggupta2005@gmail.com>
2023-06-22 21:36:25 +0000

    Change generic exception to UserErrorException
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>

--- data_understand/constants.py
+++ data_understand/constants.py
@@ -1,9 +1,6 @@
 URLS = {
-    "leaky features":
-        "https://en.wikipedia.org/wiki/Leakage_(machine_learning)",
-    "class imbalance":
-        "https://developers.google.com/machine-learning/data-prep/" +
-        "construct/sampling-splitting/imbalanced-data",
-    "Pearson correlation coefficient":
-        "https://en.wikipedia.org/wiki/Pearson_correlation_coefficient",
+    "leaky features": "https://en.wikipedia.org/wiki/Leakage_(machine_learning)",
+    "class imbalance": "https://developers.google.com/machine-learning/data-prep/"
+    + "construct/sampling-splitting/imbalanced-data",
+    "Pearson correlation coefficient": "https://en.wikipedia.org/wiki/Pearson_correlation_coefficient",
 }
--- data_understand/input_validations.py
+++ data_understand/input_validations.py
@@ -2,38 +2,41 @@ import os
 from typing import Any
 
 import pandas as pd
+from raiutils.exceptions import UserErrorException
 
 
 def validate_input_parameters(args: Any) -> None:
     if args.file_name is None:
-        raise Exception(
+        raise UserErrorException(
             "A valid file name {0} is required. "
             "Please provide a valid file path.".format(args.file_name)
         )
 
     if args.target_column is None:
-        raise Exception("A valid target column name is required.")
+        raise UserErrorException("A valid target column name is required.")
 
     if not args.file_name.endswith(".csv"):
-        raise Exception(
+        raise UserErrorException(
             "The file {} is not a CSV file. "
             "Please provide a CSV file.".format(args.file_name)
         )
 
     if not os.path.exists(args.file_name):
-        raise Exception("The file {} doesn't exists.".format(args.file_name))
+        raise UserErrorException(
+            "The file {} doesn't exists.".format(args.file_name)
+        )
 
     try:
         df = pd.read_csv(args.file_name)
     except Exception:
-        raise Exception(
+        raise UserErrorException(
             "Unable to read CSV file {0} as a pandas DataFrame".format(
                 args.file_name
             )
         )
 
     if args.target_column not in df.columns.tolist():
-        raise Exception(
+        raise UserErrorException(
             "The target column name {0} doesn't exist in dataset.".format(
                 args.target_column
             )
