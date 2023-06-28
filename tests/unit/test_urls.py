8593874aea20486e6c2560c7b97ee7df73b93351
Gaurav Gupta <ggupta2005@gmail.com>
2023-06-28 15:15:50 -0700

    Makes changes for making the repo public (#63)
    * Add long description and project URL
    
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>
    
    * Bump up version
    
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>
    
    ---------
    
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>

--- tests/unit/test_urls.py
+++ tests/unit/test_urls.py
@@ -6,9 +6,5 @@ from data_understand.constants import URLS
 class TestURLs:
     def test_urls(self):
         for key in URLS:
-            # TODO: Remove the if condition below once the
-            # repo is public.
-            if key == "data.understand":
-                continue
             response = requests.get(URLS[key])
             assert response.status_code == 200
