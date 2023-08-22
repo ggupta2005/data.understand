cd7cdfe3366d362f8b40da92869d4c2b21d02d7b
Gaurav Gupta <ggupta2005@gmail.com>
2023-08-22 17:18:06 +0000

    Add flake8-all-not-strings to linting
    Signed-off-by: Gaurav Gupta <ggupta2005@gmail.com>

--- data_understand/class_imbalance/__init__.py
+++ data_understand/class_imbalance/__init__.py
@@ -6,7 +6,7 @@ from data_understand.class_imbalance.imbalance import (
     get_message_target_column_imbalance)
 
 __all__ = [
-    get_message_target_column_imbalance,
+    "get_message_target_column_imbalance",
     "find_target_column_imbalance",
     "get_jupyter_nb_code_to_find_target_column_imbalance",
 ]
